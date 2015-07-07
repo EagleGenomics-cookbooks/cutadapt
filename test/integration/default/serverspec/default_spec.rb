73b941d5ef0d70d515736647ad7bd6d5c386758b
Bart Ailey <bart.ailey@eaglegenomics.com>
2015-07-07 17:32:04 +0100

    install working but no tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,6 +1,14 @@
-require 'spec_helper'
+require 'serverspec'
 require_relative './spec_helper'
 
 # Required by serverspec
 set :backend, :exec
 
+describe command('which cutadapt') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('cutadapt -help') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should contain ENV['CUTADAPT_VERSION'] }
+end
