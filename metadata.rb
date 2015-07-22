965c1bdaadfc215ab804337ba466b1f3040e0df1
Bart Ailey <bart.ailey@eaglegenomics.com>
2015-07-22 11:54:27 +0100

    added .kitchen.yml

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'cutadapt'
 maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer_email 'chef@eaglegenomics.com'
 license 'all_rights'
 description 'Installs/Configures cutadapt'
 long_description 'Installs/Configures cutadapt'
@@ -8,4 +8,3 @@ version '0.1.0'
 
 depends 'build-essential'
 depends 'magic_shell'
-
