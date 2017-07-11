580de2b4b553b8cb7d1b2d9c9293ceb04faaa66b
GreetDB <grdbaets@gmail.com>
2017-07-11 09:59:24 +0100

    test centos6

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,13 @@ maintainer_email 'chef@eaglegenomics.com'
 license 'Apache v2.0'
 description 'Installs/Configures cutadapt'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.1'
+version '1.0.2'
+
+source_url 'https://github.com/EagleGenomics-cookbooks/cutadapt'
+issues_url 'https://github.com/EagleGenomics-cookbooks/cutadapt/issues'
+
+supports 'ubuntu', '= 14.04'
+supports 'centos', '= 6.5'
 
 depends 'build-essential'
 depends 'magic_shell'
