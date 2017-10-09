759fa4d4714313fe439edeb993e796610d99600b
GreetDB <grdbaets@gmail.com>
2017-10-09 11:31:16 +0100

    other spec test

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,8 +7,14 @@
 require 'spec_helper'
 
 describe 'cutadapt::default' do
-  context 'When all attributes are default, on an unspecified platform' do
-    let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+  context 'When all attributes are default, on an Ubuntu 16.04' do
+    let(:chef_run) do
+      # for a complete list of available platforms and versions see:
+      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner.converge(described_recipe)
+    end
+
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
