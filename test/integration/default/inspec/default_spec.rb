369a8fde212b9e57cdc345d232ba795b0e3852cb
Nick James <nick.james@eaglegenomics.com>
2019-03-05 16:50:35 +0000

    moved file

--- test/integration/default/inspec/default_spec.rb
+++
@@ -1,9 +0,0 @@
-
-describe command('which cutadapt') do
-  its(:exit_status) { should eq 0 }
-end
-
-describe command('cutadapt -help') do
-  its(:exit_status) { should eq 0 }
-  its(:stdout) { should include('1.9.1') }
-end
