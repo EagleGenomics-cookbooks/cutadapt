4b61292b084433ce96ef17c499289575099a1856
Nick James <nick.james@eaglegenomics.com>
2019-03-05 16:23:03 +0000

    updating to tool template

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,17 +1,17 @@
 #
-# Cookbook Name:: cutadapt
+# Cookbook:: cutadapt
 # Spec:: default
 #
-# Copyright (c) 2016 Eagle Genomics Ltd, Apache License, Version 2.0.
+# Copyright:: 2019, The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
 describe 'cutadapt::default' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
+  context 'When all attributes are default, on Ubuntu 18.04' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '18.04')
       runner.converge(described_recipe)
     end
 
