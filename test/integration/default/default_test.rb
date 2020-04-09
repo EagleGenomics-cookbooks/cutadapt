80578f1403c39aaf64d68d021837c550abdebf47
nick-james <nick.james@eaglegenomics.com>
2020-04-09 15:00:59 +0100

    Update default_test.rb

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,7 +5,6 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-
 describe command('which cutadapt') do
   its(:exit_status) { should eq 0 }
 end
